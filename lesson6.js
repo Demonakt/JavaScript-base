8af0c8856c5247a2bbfdc42a6b790eef0132ef63
[Nikita Lomov] <[lomello@yandex.ru]>
2021-09-01 17:35:05 +0300

    Lesson6

--- lesson6.js
+++ lesson6.js
@@ -1,1 +1,100 @@
 'use strict'
+
+// 1-1
+
+// let test = document.querySelector('img');
+// test.src = 'images/2.jpg';
+
+// 1-2
+
+// let test = document.querySelectorAll('div')
+// test.forEach(test => test.style='padding:10px');
+
+// 1-3
+
+// let test = document.querySelectorAll('.product');
+// test.forEach(product => product.style='background-color:yellow');
+//
+// let test2 = document.querySelectorAll('.productName')
+// test2.forEach(productName => productName.style='font-size:24px')
+
+// 1-4
+
+// setTimeout(function() {
+//     alert('message')
+// }, 3000 );
+
+// 1-5
+
+// let test = document.querySelectorAll('input')
+// test.forEach(input => input.style='margin-bottom:20px')
+
+// 1-6
+
+// let test = document.querySelectorAll('input')
+// test.forEach(input => input.insertAdjacentHTML('afterend', '<br>'))
+
+// 2-1
+
+// let spanEl = document.querySelector('span')
+// let fromEl = document.getElementById('from')
+// fromEl.addEventListener('change', function (event) {
+//     spanEl.innerText = event.target.value
+// })
+
+// 2-2
+
+// let btn = document.querySelector('button')
+// let msg = document.querySelector('.message')
+// btn.addEventListener('click', function(event){
+//     msg.style='display:block';
+//     msg.classList.add('animate__animated', 'animate__fadeInLeftBig')
+// })
+
+// 2-3
+
+// let firstStar = document.getElementById('first');
+// let secondStar = document.getElementById('second');
+// let thirdStar = document.getElementById('third');
+// let fourthStar = document.getElementById('fourth');
+// let fifthStar = document.getElementById('fifth');
+//
+// let goldenClass = 'golden';
+//
+// firstStar.addEventListener('mouseover', function() {
+//     if (!firstStar.classList.contains('goldenClass')) {
+//         firstStar.classList.add(goldenClass);
+//     }
+//     firstStar.style='color:gold';
+// });
+//
+// firstStar.addEventListener('mouseout', function() {
+//     firstStar.classList.remove(goldenClass);
+//     firstStar.style='color:black';
+// });
+//
+// secondStar.addEventListener('mouseover', function(){
+//     if(!secondStar.classList.contains('goldenClass')){
+//         secondStar.classList.add(goldenClass)
+//     }
+//     firstStar.style='color:gold';
+//     secondStar.style='color:gold';
+// })
+// secondStar.addEventListener('mouseout', function() {
+//     secondStar.classList.remove(goldenClass);
+//     secondStar.style='color:black';
+//     firstStar.style='color:black';
+// });
+// ну итд
+
+// 3-1
+
+
+
+
+
+
+
+
+
+
