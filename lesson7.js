2692f12e61ffa1b5992e9d72f8ae4528eaf60ff0
[Nikita Lomov] <[lomello@yandex.ru]>
2021-09-04 23:32:26 +0300

    Lesson7

--- lesson7.js
+++ lesson7.js
@@ -1,2 +1,97 @@
 'use strict'
 
+// 1-1
+
+// for(let key in products){
+//     console.log(key)
+//     console.log(products[key])
+// }
+
+// 1-2
+
+// let test = document.querySelector('p')
+// test.innerHTML = '';
+
+// 1-3
+
+// let totalString = 'слово1' + 'слово2' + 'слово3';
+// console.log(totalString);
+
+// 1-4
+
+// let url = "https://picsum.photos/200/300";
+// let targetAttr = "_blank";
+// let linkText = "ссылка";
+//
+// document.body.insertAdjacentHTML(`afterbegin`,`<a href=${url} target=${targetAttr}>${linkText}</a>`)
+
+// 2-1
+
+// let test = document.querySelectorAll('button')
+// test.forEach(function (elem, i, test) {
+// elem.addEventListener('click', function(event){
+//     console.log(event.target.dataset.id);
+//     console.log(event.target.dataset.value);
+// })
+// })
+
+//  2-2
+
+// for( let i = 0; i < 6;i++){
+//     document.body.insertAdjacentHTML('afterbegin', `<img src="https://picsum.photos/200/300?random=${i}" alt="picture${i}" </img> `)
+// }
+
+// 2-3
+
+// let inpt = document.querySelector('input')
+// let div = document.querySelector('div')
+//
+// inpt.addEventListener('input', function (event) {
+//     console.log(event.target.value)
+//     div.style=`font-size:${event.target.value}px`
+// })
+
+// 3-1
+// в index1.html
+
+// 3-2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
